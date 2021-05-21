7058bbeadc1aa637a02cf7de9bb175265d0807b4
Vivek Anand <vivek17212797@gmail.com>
2021-05-21 22:52:04 +0530

    solved static files issue

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('',views.home),
     path('products/',views.products),
     path('customer/',views.customer),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
